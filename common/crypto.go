79c4e8fea10f5f0c8f88a5eadc2083a275900703
Alex Breadman <chainetix@gmail.com>
2022-05-12 11:23:49 +0100

    add crypto

--- common/crypto.go
+++ common/crypto.go
@@ -2,8 +2,13 @@ package common
 
 import (
 	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
 	"crypto/sha1"
 	"crypto/sha256"
+	"fmt"
+	"io"
 )
 
 func (app *App) SHA1(b []byte) []byte {
@@ -15,4 +20,39 @@ func (app *App) SHA1(b []byte) []byte {
 func (app *App) SHA256(b ...[]byte) []byte {
 	digest := sha256.Sum256(bytes.Join(b, nil))
 	return digest[:]
+}
+
+func (app *App) Encrypt(key, payload []byte) ([]byte, error) {
+	c, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	gcm, err := cipher.NewGCM(c)
+	if err != nil {
+		return nil, err
+	}
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		fmt.Println(err)
+	}
+	return gcm.Seal(nonce, nonce, payload, nil), nil
+}
+
+func (app *App) Decrypt(key, ciphertext []byte) ([]byte, error) {
+
+	c, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	gcm, err := cipher.NewGCM(c)
+	if err != nil {
+		return nil, err
+	}
+	nonceSize := gcm.NonceSize()
+	if len(ciphertext) < nonceSize {
+		return nil, err
+	}
+
+	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	return gcm.Open(nil, nonce, ciphertext, nil)
 }
