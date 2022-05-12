945da8e63e23af2f1f61f3b425b7d1cede9b2e62
Alex Breadman <chainetix@gmail.com>
2022-05-12 20:13:37 +0100

    add ascii

--- common/encoding.go
+++ common/encoding.go
@@ -1,6 +1,10 @@
 package common
 
-import "github.com/fxamacker/cbor/v2"
+import (
+	"encoding/ascii85"
+
+	"github.com/fxamacker/cbor/v2"
+)
 
 func (app *App) MarshalCBOR(x interface{}) ([]byte, error) {
 	return app.cbor.Marshal(x)
@@ -8,4 +12,14 @@ func (app *App) MarshalCBOR(x interface{}) ([]byte, error) {
 
 func (app *App) UnmarshalCBOR(b []byte, dst interface{}) error {
 	return cbor.Unmarshal(b, dst)
+}
+
+func (app *App) CompactSerial(x interface{}) (string, error) {
+	b, err := app.MarshalCBOR(x)
+	if err != nil {
+		return "", nil
+	}
+	dst := make([]byte, len(b))
+	n := ascii85.Encode(dst, b)
+	return string(dst[:n]), nil
 }
