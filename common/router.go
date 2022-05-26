163234429a61b6597e4e238fe189e60dfd1ad145
Alex Breadman <chainetix@gmail.com>
2022-05-26 16:48:37 +0100

    fix gin accessability

--- common/router.go
+++ common/router.go
@@ -24,7 +24,7 @@ func (app *App) AddRoute(method, path string, handler gin.HandlerFunc) {
 func (app *App) OptionsHandler(c *gin.Context) {
 	c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization, email, password, username")
 	c.Header("Access-Control-Allow-Origin", "*")
-	c.Header("Access-Control-Allow-Methods", "HEAD, PUT, GET, POST, DELETE")
+	c.Header("Access-Control-Allow-Methods", "HEAD, PUT, GET, POST, PATCH, DELETE")
 	if c.Request.Method == "OPTIONS" {
 		c.Status(204)
 	}
@@ -35,10 +35,10 @@ func (app *App) Serve() error {
 	filter := map[string]bool{}
 	for _, route := range app.routes {
 		if !filter[route.Path] {
-			log.Println("adding route:", route.Method, route.Path)
 			app.Gin.OPTIONS(route.Path, app.OptionsHandler)
 			filter[route.Path] = true
 		}
+		log.Println("adding route:", route.Method, route.Path)
 		app.Gin.Handle(route.Method, route.Path, app.OptionsHandler, route.Handler)
 	}
 
