9ded9df86d8c998f25bedd22ff5ffd9970fcc7b4
Alex Breadman <chainetix@gmail.com>
2022-07-21 21:28:17 +0100

    add quality checks

--- graph/client_linkNodes.go
+++ graph/client_linkNodes.go
@@ -2,10 +2,15 @@ package graph
 
 import (
 	"context"
+	"fmt"
 	"time"
 )
 
 func (client *GraphClient) LinkNodes(in, out string, predicate string, data ...map[string]interface{}) error {
+
+	if len(in) == 0 || len(out) == 0 {
+		return fmt.Errorf("one or more inputs for predicate %s are length zero", predicate)
+	}
 
 	edge := &Edge{
 		I: in,
