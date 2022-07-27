d3db37563da23ffb5d32b44e8be35cf1b756ba2d
Alex Breadman <chainetix@gmail.com>
2022-07-27 22:59:33 +0100

    add storage methd

--- common/storage.go
+++ common/storage.go
@@ -8,6 +8,14 @@ import (
 	"cloud.google.com/go/storage"
 )
 
+func (app *App) GetBucket(bucketName string) (*storage.BucketHandle, error) {
+	bucket, err := app.Storage.Bucket(bucketName)
+	if err != nil {
+		return nil, err
+	}
+	return bucket, nil
+}
+
 func (app *App) GetObject(bucket *storage.BucketHandle, objectName string) ([]byte, error) {
 	r, err := bucket.Object(objectName).NewReader(context.Background())
 	if err != nil {
